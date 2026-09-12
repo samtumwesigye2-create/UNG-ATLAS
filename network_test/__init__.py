2ec5a1d2c3696d0bd0ec8524d86b4c8335549581
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-09-12 04:05:54 -0400

    fix: align network test package exports with runner implementation

--- network_test/__init__.py
+++ network_test/__init__.py
@@ -1,5 +1,5 @@
 """UNG-ATLAS deterministic whole-network mock-test harness."""
 from .models import RunConfig
-from .runner import NetworkTestRunner
+from .runner import dispatch, run_batch, summarize
 
-__all__ = ["RunConfig", "NetworkTestRunner"]
+__all__ = ["RunConfig", "dispatch", "run_batch", "summarize"]
